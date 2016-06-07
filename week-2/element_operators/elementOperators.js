374b7b22bea4dc59bfc5a8490a4e3359a0499a1b
Willian Antunes <willian.lima.antunes@gmail.com>
2016-06-06 23:40:55 -0300

    Logical operators

--- week-2/element_operators/elementOperators.js
+++ week-2/element_operators/elementOperators.js
@@ -1,6 +1,6 @@
-db.moviesDetails.find({ "tomato.meter": { $exists: true } })
+db.movieDetails.find({ "tomato.meter": { $exists: true } })
 
-db.moviesDetails.find({ "tomato.meter": { $exists: false } })
+db.movieDetails.find({ "tomato.meter": { $exists: false } })
 
 // Value of $type may be either a BSON type number or the string alias
 // See https://docs.mongodb.org/manual/reference/operator/query/type
