a8f9dfa1a549a0603def5be51e1ce60de51b572b
Willian Antunes <willian.lima.antunes@gmail.com>
2016-06-15 08:58:44 +0000

    Java driver quiz about delete function

--- m101j_week2_examples/src/main/java/com/mongodb/m101j/crud/App.java
+++ m101j_week2_examples/src/main/java/com/mongodb/m101j/crud/App.java
@@ -90,6 +90,7 @@ public class App {
     	MongoClient client = new MongoClient();
         MongoDatabase db = client.getDatabase("course");
         MongoCollection<Document> collection = db.getCollection("insertTestQuizSortSkipLimit");
+        collection.drop();
         
     	Document id0 = new Document("_id", 0).append("value", 10);
     	Document id1 = new Document("_id", 1).append("value", 5);
@@ -102,9 +103,27 @@ public class App {
     	all.forEach(i -> printJson(i));
     	*/
     	
+    	/*
     	MongoCollection<Document> scores = null;
     	
-    	scores.updateOne(new Document("_id", 1),  null);
+    	scores.updateOne(new Document("_id", 1), new Document("$set", new Document("examiner", "Jones")));
+    	scores.updateOne(Filters.eq("_id", 1), new Document("$set", new Document("examiner", "Jones")));
+    	*/
+    	
+       	MongoClient client = new MongoClient();
+        MongoDatabase db = client.getDatabase("course");
+        MongoCollection<Document> collection = db.getCollection("insertTestQuizDelete");
+        collection.drop();
+        
+    	Document id0 = new Document("_id", 0).append("x", 1);
+    	Document id1 = new Document("_id", 1).append("x", 1);
+    	Document id2 = new Document("_id", 2).append("x", 1);
+    	Document id3 = new Document("_id", 3).append("x", 2);
+    	Document id4 = new Document("_id", 4).append("x", 2);
+    	
+    	collection.insertMany(asList(id0, id1, id2, id3, id4));
+    	collection.deleteOne(Filters.eq("x", 1));
+    	collection.find().into(new ArrayList<Document>()).forEach(i -> printJson(i));;
     }
     
 }
